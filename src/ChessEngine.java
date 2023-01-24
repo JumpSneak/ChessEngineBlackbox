d9fb8c402d04e812dc5c8ff58d477800d2833502
Michal <mich.gor2004@gmail.com>
2023-01-24 02:38:28 +0100

    semi legal moves for all pieces (only on turn and not capturing own piece

--- src/ChessEngine.java
+++ src/ChessEngine.java
@@ -12,22 +12,44 @@ public class ChessEngine {
     int[][] board = new int[WIDTH][HEIGHT];
     List<String> moveNotation = new ArrayList<>();
 
-    enum Piece {NOP, KING, QUEEN, BISHOP, KNIGHT, ROOK, PAWN};
+    public enum Piece {NOP, KING, QUEEN, BISHOP, KNIGHT, ROOK, PAWN}
+    final public int[] piece = {0, 1, 2, 3, 4, 5, 6};
 
-    public ChessEngine(){
+
+
+    public ChessEngine() {
         initBoard();
     }
 
     // private Tool Methods
-    private void placeNewPiece(int x, int y, int id, boolean team){
+    public int getPiece(int id){
+        return id & 0x7;
+    }
+    private boolean getTeam(int id) {
+        // gets team of id
+        return getBit(id, 3) == 1;
+    }
+    private void setBoardOn(int x, int y, int id){
+        board[x][y] = id;
+    }
+    private void clearTile(int x, int y){
+        // clears tile
+        board[x][y] = 0;
+    }
+    private void placeNewPiece(int x, int y, int id, boolean team) {
+        // creates new pieces and places
         board[x][y] = createNewPiece(id, team);
     }
+
     private int createNewPiece(int id, boolean team) {
+        // creates new player with id
         int result = id; // set piece
         result = setBit(result, 3, team ? 1 : 0); // set team
         return result;
     }
+
     private void initBoard() {
+        // setup new chessboard
         for (int i = 0; i < WIDTH; i++) {
             placeNewPiece(i, 1, Piece.PAWN.ordinal(), true);
             placeNewPiece(i, 6, Piece.PAWN.ordinal(), false);
@@ -54,6 +76,27 @@ public class ChessEngine {
         placeNewPiece(4, 7, Piece.KING.ordinal(), false);
     }
 
+    // private Piece Move Validation Methods
+    private boolean legalMoveKing(int oldX, int oldY, int x, int y){
+        return false;
+    }
+    private boolean legalMoveQueen(int oldX, int oldY, int x, int y){
+        return false;
+    }
+    private boolean legalMoveBishop(int oldX, int oldY, int x, int y){
+        return false;
+    }
+    private boolean legalMoveKnight(int oldX, int oldY, int x, int y){
+        return false;
+    }
+    private boolean legalMoveRook(int oldX, int oldY, int x, int y){
+        return false;
+    }
+    private boolean legalMovePawn(int oldX, int oldY, int x, int y){
+        return false;
+    }
+
+
     // private Helper Methods
     private int setBit(int number, int idx, int value) {
         if (value != 0 && value != 1) {
@@ -79,32 +122,80 @@ public class ChessEngine {
         return turn % 2 == 1;
     }
 
-    public List<String> getBoardLAN() {
-        return moveNotation;
-    }
-
     public boolean canMove(int oldX, int oldY, int x, int y) {
-        return false; // TODO
+        // TODO major and most important
+        // check turn
+        if (getTurn() != getTeam(getBoardOn(oldX, oldY))) {
+            return false;
+        }
+        // check if newpos empty or opponent
+        int zwNewId = getBoardOn(x, y);
+        if(zwNewId != 0 && getTeam(zwNewId) == getTurn()){
+            return false;
+        }
+        // check if legal piece move
+        int zwOldId = getBoardOn(oldX, oldY);
+        switch (Piece.values()[getPiece(zwOldId)]){
+            case KING:
+                System.out.println("King");
+                break;
+            case QUEEN:
+                System.out.println("queen");
+                break;
+            case BISHOP:
+                System.out.println("bishop");
+                break;
+            case KNIGHT:
+                System.out.println("knight");
+                break;
+            case ROOK:
+                System.out.println("rook");
+                break;
+            case PAWN:
+                System.out.println("pawn");
+                break;
+            default:
+                System.out.println("ERROR: original Piece not found");
+                break;
+        }
+
+        // check if in check after move
+
+        return true;//temp
     }
 
     public boolean makeMove(int oldX, int oldY, int x, int y) {
-        return false; // TODO
+        if (canMove(oldX, oldY, x, y)) {
+            int zwId = getBoardOn(oldX, oldY);
+            setBoardOn(x, y, zwId);
+            clearTile(oldX, oldY);
+            turn++;
+            // (TODO other stuff to update when making a move)
+            return true;
+        } else {
+            return false;
+        }
     }
 
-    public boolean setBoard(List<String> input) {
-        return false; // TODO
+    public List<String> getBoardLAN() {
+        return moveNotation; // TODO later later
+    }
+
+    public boolean setBoardLAN(List<String> input) {
+        return false; // TODO for later later
     }
 
     // public testing methods
-    public void printBoard(){
-        for (int i = HEIGHT-1; i >= 0; i--) {
+    public void printBoard() {
+        for (int i = HEIGHT - 1; i >= 0; i--) {
             for (int j = 0; j < WIDTH; j++) {
                 String a = Integer.toString(board[j][i], 2);
+//                String a = Integer.toString(getPiece(getBoardOn(j, i)), 2);
                 String res = "";
-                for (int k = 0; k < 5-a.length(); k++) {
+                for (int k = 0; k < 5 - a.length(); k++) {
                     res += " ";
                 }
-                System.out.print(res+ a + "|");
+                System.out.print(res + a + "|");
             }
             System.out.println("\n-----------------------------------------------");
         }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         ChessEngine chessEngine = new ChessEngine();
-        chessEngine.printBoard();
+        while (true) {
+            chessEngine.printBoard();
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("from (x [space] y)");
+            int fromx = scanner.nextInt();
+            int fromy = scanner.nextInt();
+            System.out.println(fromx + " kk " + fromy +"To (x [space] y)");
+            int tox = scanner.nextInt();
+            int toy = scanner.nextInt();
+            System.out.println(chessEngine.makeMove(fromx, fromy, tox, toy));
+
+        }
     }
 }
